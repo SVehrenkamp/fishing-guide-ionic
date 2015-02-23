b96c6ed9eecfa1f0a5331dd534579f419320093c
Scott.Vehrenkamp <scott.vehrenkamp@target.com>
2015-02-22 22:26:00 -0600

    Updated Client/Server Path

--- server/server/server.js
+++ server/server/server.js
@@ -11,7 +11,7 @@ boot(app, __dirname);
 
 var path = require('path');
 //App Entry Point index.html file
-app.use(loopback.static(path.resolve(__dirname, '../client')));
+app.use(loopback.static(path.resolve(__dirname, '../../root')));
 
 app.start = function() {
   // start the web server
